ec3c8c1ad67d6e3a3c42cbe37694561be0b2bddd
Uryarajesh <Uryarajesh@gmail.com>
2022-03-22 15:00:18 +0530

    updated my code

---
+++ 1.java
@@ -0,0 +1,12 @@
+class Arrary
+{
+
+public static void main (String[]args)
+{
+   int marks[] = {26,78,56,76,99};   
+  for(int i= 0; i<marks[].length;i ++)
+  {
+  System.out.println("10 th mark is"+marks[i]);
+  }
+  }
+  }
