dbbbe56c2d567a51bb50a2f3656096454d154163
Uryarajesh <Uryarajesh@gmail.com>
2022-04-04 17:39:32 +0530

    update my name to design

---
+++ Alphabet.java
@@ -0,0 +1,301 @@
+package tamilnadu.chennai;
+
+public class Alphabet {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+          Alphabet aa=new Alphabet();
+       
+	aa.nameR();
+	aa.nameA();
+	aa.nameJ();
+    aa.nameE();
+    aa.nameS();
+    aa.nameH();
+	  
+	}
+	private void nameR() {
+		// TODO Auto-generated method stub
+		for(int row=1;row<=7;row++)
+		{
+		for(int col=1;col<=7;col++)
+		{ 
+		if(col==1||col==7) {
+
+		System.out.print("*   ");
+		}
+		else {
+		System.out.print("  ");
+		}
+		}
+		System.out.println();	
+		
+		//public void name()
+		{ 
+		for(int col=1;col<=7;col++)
+		{
+		System.out.print("* ");
+		}
+		System.out.println();
+		}
+		}
+		
+		for(int row=1;row<=7;row++)
+		{
+		for(int col=1;col<=7;col++)
+		{ 
+		if(col==1||col==7) {
+
+		System.out.print("*   ");
+		}
+		else {
+		System.out.print("  ");
+		}
+		}
+		System.out.println();	
+
+		}
+		for(int col=1;col<=7;col++)
+		{
+		System.out.print("* ");
+		}
+		System.out.println();
+		}
+		
+
+	private void nameA() {
+	// TODO Auto-generated method stub
+	for(int row=1;row<=7;row++)
+	{
+	for(int col=1;col<=7;col++)
+	{ 
+	if(col==1||col==7) {
+
+	System.out.print("*   ");
+	}
+	else {
+	System.out.print("  ");
+	}
+	}
+	System.out.println();	
+	
+	//public void name()
+	{ 
+	for(int col=1;col<=7;col++)
+	{
+	System.out.print("* ");
+	}
+	System.out.println();
+	}
+	}
+	
+	for(int row=1;row<=7;row++)
+	{
+	for(int col=1;col<=7;col++)
+	{ 
+	if(col==1||col==7) {
+
+	System.out.print("*   ");
+	}
+	else {
+	System.out.print("  ");
+	}
+	}
+	System.out.println();	
+
+	}
+	for(int col=1;col<=7;col++)
+	{
+	System.out.print("* ");
+	}
+	System.out.println();
+	}
+	
+
+
+private void nameS() {
+		// TODO Auto-generated method stub
+	for(int row=1;row<=7;row++)
+	{
+	for(int col=1;col<=7;col++)
+	{ 
+		if(col==7) {
+	
+		System.out.print("* ");
+		}
+	else {
+		System.out.print("  ");
+	}
+	}
+		System.out.println();	
+	
+	}
+	for(int col=1;col<=7;col++)
+	{
+	System.out.print("* ");
+	}
+	System.out.println();
+
+//public void names()
+//{ 
+	for(int col=1;col<=7;col++)
+	{
+	System.out.print("* ");
+	}
+	System.out.println();
+	for(int row=1;row<=7;row++)
+	{
+	for(int col=1;col<=7;col++)
+	{ 
+		if(col==1) {
+	
+		System.out.print("* ");
+		}
+	else {
+		System.out.print("  ");
+	}
+	}
+		System.out.println();	
+	
+	}
+	for(int col=1;col<=7;col++)
+	{
+	System.out.print("* ");
+	}
+	System.out.println();
+}
+	
+
+	
+	private void nameE() {
+		// TODO Auto-generated method stub
+	for(int row=1;row<=7;row++)
+	{
+	for(int col=1;col<=7;col++)
+	{ 
+		if(col==1) {
+	
+		System.out.print("* ");
+		}
+	else {
+		System.out.print("  ");
+	}
+	}
+		System.out.println();	
+	
+	}
+	for(int col=1;col<=7;col++)
+	{
+	System.out.print("* ");
+	}
+	System.out.println();
+}
+//public void names4()
+{ 
+
+	for(int row=1;row<=7;row++)
+	{
+	for(int col=1;col<=7;col++)
+	{ 
+		if(col==1) {
+	
+		System.out.print("* ");
+		}
+	else {
+		System.out.print("  ");
+	}
+	}
+		System.out.println();	
+	
+	}
+	for(int col=1;col<=7;col++)
+	{
+	System.out.print("* ");
+	}
+	System.out.println();
+}
+
+	private void nameH() {
+		// TODO Auto-generated method stub
+	for(int row=1;row<=7;row++)
+	{
+	for(int col=1;col<=7;col++)
+	{ 
+		if(col==1|| col==7) {
+	
+		System.out.print("* ");
+		}
+	else {
+		System.out.print("  ");
+	}
+	}
+		System.out.println();	
+	
+	}
+	for(int col=1;col<=7;col++)
+	{
+	System.out.print("* ");
+	}
+	System.out.println();
+}
+//public void names6()
+{ 
+
+	for(int row=1;row<=7;row++)
+	{
+	for(int col=1;col<=7;col++)
+	{ 
+		if(col==1|| col==7) {
+	
+		System.out.print("* ");
+		}
+	else {
+		System.out.print("  ");
+	}
+	}
+		System.out.println();	
+	
+	}
+	System.out.println();
+}
+	//for(int col=1;col<=7;col++)
+	//{
+	//System.out.print("* ");
+	//}
+	private void nameJ() {
+		// TODO Auto-generated method stub
+	for(int row=1;row<=7;row++)
+	{
+		System.out.print("* ");
+	}
+		for(int row =1;row<=7;row++)
+		{
+	for(int col=1;col<=7;col++)
+	{ 
+		if( col==4) {
+	
+		System.out.print("* ");
+		}
+	else {
+		System.out.print("  ");
+	}
+	}
+		System.out.println();
+		}
+for( int col=1;col<=4;col++) 
+	System.out.print("* "); 
+}
+	
+}
+
+
+
+
+
+
+
+	
+	
+
+
+
